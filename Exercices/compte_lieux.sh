e37887a4e1a4e14ad667a640814bcc20daab2515
MrnMartin <151888353+MrnMartin@users.noreply.github.com>
2024-10-30 15:58:05 +0100

    Create compte_lieux.sh

---
+++ Exercices/compte_lieux.sh
@@ -0,0 +1,13 @@
+#/usr/bin/bash 
+
+doss="/home/mrinab/Exercice1/ann"
+
+echo "<year> <month> <nb>"
+exit 1 
+fi
+
+year=$1
+month=$2
+nb=$3
+
+cat 201*/*_10_*.ann | grep  "Location" ${year}/${month}*.ann | cut -f 3 | sort | uniq -c | sort | tail -n15 | sort -r $nb
