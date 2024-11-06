a849c364ed01e691ab543297e79511003324cf3a
MrnMartin <151888353+MrnMartin@users.noreply.github.com>
2024-11-06 10:10:15 +0100

    Update compte_lieux.sh

--- Exercices/compte_lieux.sh
+++ Exercices/compte_lieux.sh
@@ -1,15 +1,19 @@
 #/usr/bin/bash 
 
-doss="/home/mrinab/Exercice1/ann"
-
-if [ $# -ne 1 ]
+if[ $# -ne 3 ]
 then
-echo " $ 0 <year> <month> <nb>"
-exit 1 
+  echo "Besoin de 3 arguments"
+  exit 1 
 fi
 
-year=$1
-month=$2
+annee=$1
+mois=$2
 nb=$3
 
-cat 201*/*_10_*.ann | grep  "Location" ${year}/${month}*.ann | cut -f 3 | sort | uniq -c | sort | tail -n15 | sort -r $nb
+if [$nb -lt 1]
+then 
+  echo "Au moins un lieu" 
+  exit 2
+fi 
+
+cat 201*/*_10_*.ann | grep  "Location" ${annee}/${mois}_*.ann | cut -f 3 | sort | uniq -c | sort | tail -n15 | sort -r $nb
