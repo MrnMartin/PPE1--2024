c1ab9ffb520346c9342638f1564e17dbf469896d
MrnMartin <151888353+MrnMartin@users.noreply.github.com>
2024-11-06 09:27:54 +0100

    Update comptes.sh

--- Exercices/comptes.sh
+++ Exercices/comptes.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/bash
 
-d1="/home/mrinab/Exercice1/ann"
+echo "Location en 2016:"
+grep Location ~Exercice1/ann/2016/*.ann | wc -l 
 
-for year in 2016 2017 2018; do 
-echo "pour l'année $year"
-grep -c "Location" "$d1/$year"*.ann|wc-l
+echo "Location en 2017:"
+grep Location ~Exercice1/ann/2017/*.ann | wc -l 
+
+echo "Location en 2018:"
+grep Location ~Exercice1/ann/2018/*.ann | wc -l 
+
+for annee in 2016 2017 2018 
+do
+	echo "Location en $annee"
+	grep Location ~/Exercice1/ann/$annee/*.ann | wc -l
 done
